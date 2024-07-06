525b16471b6337e54e3d2093992e58984803a2e6
a.alkhanov <a.alkhanov@ngrsoftlab.ru>
2024-07-06 11:32:50 +0300

    asdasd

---
+++ assets/script.js
@@ -0,0 +1,568 @@
+const config = {
+	pr: {
+		name: 'PR&Team',
+		items: {
+			support: {
+				name: 'Support team',
+				value: 0
+			},
+			hb: {
+				name: 'HamsterBooks',
+				value: 0
+			},
+			x: {
+				name: 'X',
+				value: 0
+			},
+			cointelegraph: {
+				name: 'Cointelegraph',
+				value: 0
+			},
+			ht: {
+				name: 'HamsterTube',
+				value: 0
+			},
+			hg: {
+				name: 'HamsterGram',
+				value: 0
+			},
+			tiktok: {
+				name: 'TikTok',
+				value: 0
+			},
+			coindesk: {
+				name: 'Coindesk',
+				value: 0
+			},
+			influencers: {
+				name: 'Influencers',
+				value: 0
+			},
+			ceo: {
+				name: 'CEO',
+				value: 0
+			},
+			it: {
+				name: 'IT team',
+				value: 0
+			},
+			marketing: {
+				name: 'Marketing',
+				value: 0
+			},
+			partnership: {
+				name: 'Partnership program',
+				value: 0
+			},
+			pt: {
+				name: 'Product team',
+				value: 0
+			},
+			bigdev: {
+				name: 'BisDev team',
+				value: 0
+			},
+			f2a: {
+				name: 'Two factor authentication'
+			},
+			uxui: {
+				name: 'UX and UI team'
+			},
+			security: {
+				name: 'Security team',
+			},
+			qa: {
+				name: 'QA team'
+			},
+			shield: {
+				name: 'Antihacking shield',
+			},
+			risk: {
+				name: 'Risk management team'
+			},
+			audit: {
+				name: 'Security Audition'
+			},
+			txban: {
+				name: 'Anonymous transactions ban'
+			},
+			block: {
+				name: 'Blocking suspicious accounts'
+			},
+			te: {
+				name: 'Tokenomis expert'
+			},
+			cep: {
+				name: 'Consensus Explorer pass'
+			},
+			vclabs: {
+				name: 'VC Labs'
+			},
+			comoffiser: {
+				name: 'Compliance offiser'
+			},
+			amster: {
+				name: 'Welcome to Amsterdam'
+			},
+			mumbai: {
+				name: 'Development Hub Mumbai'
+			},
+			tokio: {
+				name: 'Data Center Tokyo'
+			}
+		}
+	},
+	markets: {
+		name: 'Markets',
+		items: {
+			fan: {
+				name: 'Fan tokens',
+				value: 0
+			},
+			staking: {
+				name: 'Staking',
+				value: 0
+			},
+			btc: {
+				name: 'BTC Pairs',
+				value: 0
+			},
+			eth: {
+				name: 'ETH Pairs',
+				value: 0
+			},
+			cmc: {
+				name: 'Top 10 cmc pairs',
+				value: 0
+			},
+			gamefi: {
+				name: 'GameFi tokens',
+				value: 0
+			},
+			defi20: {
+				name: 'Defi2.0 tokens',
+				value: 0
+			},
+			socialfi: {
+				name: 'SocialFi tokens',
+				value: 0
+			},
+			meme: {
+				name: 'Meme coins',
+				value: 0
+			},
+			shit: {
+				name: 'Shit coins',
+				value: 0
+			},
+			x10: {
+				name: 'Margin trading x10'
+			},
+			x20: {
+				name: 'Margin trading x20'
+			},
+			x30: {
+				name: 'Margin trading x30'
+			},
+			x50: {
+				name: 'Margin trading x50'
+			},
+			x75: {
+				name: 'Margin trading x75'
+			},
+			x100: {
+				name: 'Margin trading x100'
+			},
+			derivatives: {
+				name: 'Derivatives'
+			},
+			pmarkets: {
+				name: 'Prediction markets'
+			},
+			web3: {
+				name: 'Web3 integration'
+			},
+			dao: {
+				name: 'DAO'
+			},
+			p2p: {
+				name: 'P2P trading'
+			},
+			bots: {
+				name: 'Trading bots'
+			},
+			layer: {
+				name: 'LayerZero Listing'
+			}
+		}
+	},
+	legal: {
+		name: 'Legal',
+		items: {
+			kyc: {
+				name: 'KYC',
+				value: 0
+			},
+			kyb: {
+				name: 'KYB',
+				value: 0
+			},
+			legal: {
+				name: 'Legal opinion',
+				value: 0
+			},
+			sec: {
+				name: 'SEC transparency',
+				value: 0
+			},
+			aml: {
+				name: 'Anti money loundering',
+				value: 0
+			},
+			luae: {
+				name: 'License UAE',
+				value: 0
+			},
+			leu: {
+				name: 'License Europe',
+				value: 0
+			},
+			lasia: {
+				name: 'License Asia',
+				value: 0
+			},
+			lsa: {
+				name: 'License South America',
+				value: 0
+			},
+			lau: {
+				name: 'License Australia',
+				value: 0
+			},
+			lna: {
+				name: 'License North America',
+				value: 0
+			},
+			ln: {
+				name: 'License Nigeria'
+			},
+			jp: {
+				name: 'License Japan'
+			},
+			ethiopia: {
+				name: 'License Ethiopia'
+			},
+			india: {
+				name: 'License Inddia'
+			},
+			bangladesh: {
+				name: 'License Bangladesh'
+			},
+			indonesia: {
+				name: 'License Indonesia'
+			},
+			vietnam: {
+				name: 'License Vietnam'
+			},
+			turkey: {
+				name: 'License Turkey'
+			},
+			philippines: {
+				name: 'License Philippines'
+			}
+		}
+	},
+	specials: {
+		name: 'Specials',
+		items: {
+			/*t49: {
+				name: 'Token2049',
+				is_active: Date.now() / 1000 < 1713960000
+			},*/
+			tonusdt: {
+				name: 'USDT on TON',
+				config: {
+					start: {
+						cost: 10000,
+						profit: 1350
+					},
+					modifier: {
+						cost: 0,
+						profit: 6.99
+					}
+				}
+			},
+			bogdanoff: {
+				name: 'Bogdanoff is calling'
+			},
+			appscl: {
+				name: 'Apps Center Listing'
+			},
+			villa: {
+				name: 'Villa for the DEV team'
+			},
+			long: {
+				name: 'Long squeeze'
+			},
+			short: {
+				name: 'Short squeeze',
+			},
+			conference: {
+				name: 'Special Hamster Conference'
+			},
+			twochairs: {
+				name: 'There are two chairs...'
+			},
+			top10: {
+				name: 'Top 10 Global Ranking'
+			},
+			nftl: {
+				name: 'NFT Collection Launch'
+			},
+			pizza: {
+				name: 'Bitcoin Pizza Day'
+			},
+			youtube: {
+				name: 'Hamster YouTube Channel'
+			},
+			goldbutton: {
+				name: 'YuoTube Gold Button'
+			},
+			web3academy: {
+				name: 'Web3 academy launch'
+			},
+			consensus: {
+				name: 'Consensus Piranha Pass'
+			},
+			tonplus: {
+				name: 'TON + Hamster Kombat = Success'
+			},
+			merch: {
+				name: 'Hamster Kombat merch'
+			},
+			premarket: {
+				name: 'Premarket Launch'
+			},
+			leaders: {
+				name: 'TG Leaders'
+			},
+			twofivemillion: {
+				name: 'YouTube 25 Million'
+			},
+			greenenergy: {
+				name: 'Hamster green energy'
+			},
+			networkx: {
+				name: 'X Network 10 Million'
+			},
+			web3game: {
+				name: 'Web3 Game Con'
+			},
+			breakrecords: {
+				name: 'Hamster break records'
+			},
+			istambul: {
+				name: 'CX Hub Istanbul'
+			},
+			winline: {
+				name: 'Winline'
+			}
+		}
+	}
+};
+
+const calculateTimeToBillion = () => {
+    const balance = Number(document.getElementById('current-balance').value * 1000000);
+    const profitPerHour = Number(document.getElementById('profit-per-hour').value);
+    const targetBalance = 1000000000; // 1 миллиард
+    const formatter = Intl.NumberFormat('en', { notation: 'compact' });
+
+    if (profitPerHour > 0) {
+        const totalSecondsToReachTarget = (targetBalance - balance) / (profitPerHour / 3600);
+        const daysToReachTarget = Math.floor(totalSecondsToReachTarget / 86400); // 86400 секунд в дне
+        const remainingSecondsAfterDays = totalSecondsToReachTarget % 86400;
+        const hoursToReachTarget = Math.floor(remainingSecondsAfterDays / 3600); // 3600 секунд в часе
+        const remainingSecondsAfterHours = remainingSecondsAfterDays % 3600;
+        const minutesToReachTarget = Math.floor(remainingSecondsAfterHours / 60); // 60 секунд в минуте
+        const secondsToReachTarget = Math.floor(remainingSecondsAfterHours % 60);
+
+        const currentDate = new Date();
+        const targetDate = new Date(currentDate.getTime() + totalSecondsToReachTarget * 1000);
+
+        const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false };
+        const targetDateString = targetDate.toLocaleString('en-US', options);
+
+        document.getElementById('time-to-billion').innerText = `${formatter.format(totalSecondsToReachTarget / 3600)} часов или ${daysToReachTarget} дней ${hoursToReachTarget} часов ${minutesToReachTarget} минут ${secondsToReachTarget} секунд\nОжидаемая дата достижения: ${targetDateString}`;
+    } else {
+        document.getElementById('time-to-billion').innerText = 'Please enter a profit per hour greater than 0';
+    }
+    saveCalculatorData();
+};
+
+
+
+const loadCalculatorData = () => {
+    const savedData = localStorage.getItem('calculatorData');
+    if (savedData) {
+        const { balance, profitPerHour } = JSON.parse(savedData);
+        document.getElementById('current-balance').value = balance || 0;
+        document.getElementById('profit-per-hour').value = profitPerHour || 0;
+        calculateTimeToBillion();
+    }
+};
+const saveCalculatorData = () => {
+    const balance = document.getElementById('current-balance').value;
+    const profitPerHour = document.getElementById('profit-per-hour').value;
+    localStorage.setItem('calculatorData', JSON.stringify({ balance, profitPerHour }));
+};
+
+document.getElementById('current-balance').addEventListener('input', calculateTimeToBillion);
+document.getElementById('profit-per-hour').addEventListener('input', calculateTimeToBillion);
+
+const formatter = Intl.NumberFormat('en', { notation: 'compact' });
+const dump = document.querySelector('textarea[name="dump"]');
+
+dump.value = localStorage.getItem('dump')?.length ? localStorage.getItem('dump') : '';
+
+const boot = () => {
+	const saved = dump.value.length ? JSON.parse(dump.value) : {};
+
+	for(const key in config) {
+		const $template = document.createElement('template');
+
+		$template.innerHTML = `<div class="category" data-key><div class="name"></div><div class="items"></div></div>`;
+		$template.content.querySelector('.category').dataset.key = key;
+		$template.content.querySelector('.name').innerText = config[key].name;
+
+		document.querySelector('.calculator .categories').append($template.content);
+
+		const $items = document.querySelector(`.categories .category[data-key="${key}"] .items`);
+
+		for(const k in config[key].items) {
+			const i = config[key].items[k];
+
+			const cost = key in saved && k in saved[key] ? saved[key][k][0] : 0;
+			const profit = key in saved && k in saved[key] ? saved[key][k][1] : 0;
+
+			$items.insertAdjacentHTML(
+				'beforeend',
+				`<div class="item" data-key="${k}">
+                    <div class="name">${i.name}</div>
+                    <div class="opinions">
+						<div>
+							<label>Upgrade cost</label>
+							<input type="number" name="cost" value="${cost}" onkeyup="update();calculate()" onchange="update();calculate()" pattern="[0-9]*" inputmode="numeric" />
+						</div>
+						<div>
+							<label>Profit/hour</label>
+							<input type="number" name="profit" value="${profit}" onkeyup="update();calculate()" onchange="update();calculate()" pattern="[0-9]*" inputmode="numeric" />
+						</div>
+					</div>
+                </div>`
+			);
+		}
+	}
+
+	calculate();
+};
+
+const update = () => {
+    const saved = {};
+
+    document.querySelectorAll('.calculator .categories .category').forEach((element) => {
+        saved[element.dataset.key] = {};
+
+        element.querySelectorAll('.items .item').forEach((el) => {
+            saved[element.dataset.key][el.dataset.key] = [
+                Number(el.querySelector('[name="cost"]').value || 0), Number(el.querySelector('[name="profit"]').value || 0)
+            ];
+        });
+    });
+
+    dump.value = JSON.stringify(saved);
+    store();
+};
+
+const calculate = () => {
+    let best;
+    const topResults = [];
+
+    document.querySelectorAll('.calculator .categories .category').forEach((element) => {
+        element.querySelectorAll('.items .item').forEach((el) => {
+            const cost = Number(el.querySelector('[name="cost"]').value);
+            const profit = Number(el.querySelector('[name="profit"]').value);
+
+            if((!best || best.value < profit / cost) && cost > 0 && profit > 0) {
+                best = {
+                    category: element.dataset.key,
+                    item: el.dataset.key,
+                    value: profit > 0 ? profit / cost : 0,
+                    cost, profit
+                };
+            }
+
+            if(cost > 0 && profit > 0) {
+                topResults.push({
+                    name: config[element.dataset.key].items[el.dataset.key].name,
+                    value: profit / cost,
+                    cost, profit
+                });
+            }
+        });
+    });
+
+	topResults.sort((a, b) => b.value - a.value);
+
+    const topResultsList = document.getElementById('top-results');
+    topResultsList.innerHTML = '';
+
+    topResults.slice(0, 95).forEach(result => {
+        const li = document.createElement('li');
+        li.textContent = `${result.name} (${formatter.format(result.cost / result.profit)} часов или ${formatter.format(result.cost / result.profit / 24)} дней)`;
+        topResultsList.appendChild(li);
+    });
+
+    if(best) {
+        document.querySelector('.calculator .categories .category .items .item.best')?.classList?.remove('best');
+        document.querySelector(`.calculator .categories .category[data-key="${best.category}"] .items .item[data-key="${best.item}"]`).classList.add('best');
+
+        return;
+    }
+
+    document.querySelector('.results').innerText = `Fill data before start`;
+};
+
+const fill = () => {
+    const saved = dump.value.length ? JSON.parse(dump.value) : {};
+
+    document.querySelectorAll('.calculator .categories .category').forEach((element) => {
+        element.querySelectorAll('.items .item').forEach((el) => {
+            el.querySelector('[name="cost"]').value = element.dataset.key in saved && el.dataset.key in saved[element.dataset.key] ? saved[element.dataset.key][el.dataset.key][0] : 0;
+            el.querySelector('[name="profit"]').value = element.dataset.key in saved && el.dataset.key in saved[element.dataset.key] ? saved[element.dataset.key][el.dataset.key][1] : 0;
+        });
+    });
+
+    store();
+    calculate();
+}
+
+const store = () => {
+    if(!dump.value.length) {
+        localStorage.removeItem('dump');
+        return;
+    }
+
+    try {
+        if(Object.prototype.toString.call(JSON.parse(dump.value)) === '[object Object]') {
+            localStorage.setItem('dump', dump.value);
+        }
+    }
+    catch(e) {
+        console.error(e.message);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadCalculatorData();
+    boot();
+});
